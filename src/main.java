27a66286a13ebf90f4e8a5163210c0f1fe421978
Maksimova <akai.gumba@gmail.com>
2026-01-19 21:54:00 +0400

    change Main.java

--- src/main.java
+++ src/main.java
@@ -1,8 +1,8 @@
-public class main {
+public class Main {
     public static void main(String[] args) {
         int ticketCost = 13676;
         int mileCost = 20;
-        int bonus =  ticketCost / mileCost;
+        int bonus = ticketCost / mileCost;
 
         System.out.println("Итоговый бонус: " + bonus + " мили");
 
